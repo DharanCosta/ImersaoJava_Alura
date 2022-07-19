9efb00d310ebf82b93e1277c14aa673988fa00f1
Dharan Costa <dharancosta@gmail.com>
2022-07-19 16:28:54 -0300

    Aula 02 - Leitura da imagem, setar tamanho fixo de imagem, criei uma nova imagem com transparência, adicionei texto e imagem sobre o poster do filme

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -13,6 +15,7 @@ public class App {
 
         // Acessar os dados da API pelo método HTTP e alocar a resposta dentro de uma String
         String url = "https://alura-imdb-api.herokuapp.com/movies";
+//        String url = "https://api.mocki.io/v2/549a5d8b/MostPopularMovies";
         URI endereco = URI.create(url);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
@@ -27,20 +30,43 @@ public class App {
         // System.out.println(listaDeFilmes.get(0));
         
         //Exibir e manipular os dados
+          //Lista de Título, LinkImagem, Classificação e estrelas no terminal
+//        for(Map<String,String> filme : listaDeFilmes){
+//
+////            System.out.println("\u001b[38;2;240;245;179m Título: \u001b[0m \u001b[1m"+filme.get("title")+"\u001b[0m");
+////            System.out.println("\u001b[38;2;240;245;179m Poster: \u001b[0m"+filme.get("image"));
+//
+////            System.out.println("\u001b[42m \u001b[34;1m Classificação: "+filme.get("imDbRating")+" \u001b[0m");
+////            int ratingInt = Math.round(rating);
+////            for(int i=0; i<ratingInt; i++ ){
+////                System.out.print("\u2b50");
+////            }
+////            System.out.println("\n");
+//        }
 
-        for(Map<String,String> filme : listaDeFilmes){
-            System.out.println("\u001b[38;2;240;245;179m Título: \u001b[0m \u001b[1m"+filme.get("title")+"\u001b[0m");
-            System.out.println("\u001b[38;2;240;245;179m Poster: \u001b[0m"+filme.get("image"));
-            
-            System.out.println("\u001b[42m \u001b[34;1m Classificação: "+filme.get("imDbRating")+" \u001b[0m");
-        
+
+        for(Map<String,String> filme : listaDeFilmes) {
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
             float rating = Float.parseFloat(filme.get("imDbRating"));
-            int ratingInt = Math.round(rating);
-            for(int i=0; i<ratingInt; i++ ){
-                System.out.print("\u2b50");
+
+            if(urlImagem.contains("@")) {
+                //substring
+                String subUrlImg = urlImagem.substring(0, urlImagem.lastIndexOf("@") + 1);
+                System.out.println(subUrlImg + ".jpg");
+
+                InputStream inputStream = new URL(subUrlImg).openStream();
+                String nomeArquivo = titulo + ".png";
+
+                GeradorDeStickers gerador = new GeradorDeStickers();
+                gerador.create(inputStream, nomeArquivo, rating);
+            }else{
+                InputStream inputStream = new URL(urlImagem).openStream();
+                System.out.println(urlImagem);
+                String nomeArquivo = titulo + ".png";
+                GeradorDeStickers gerador = new GeradorDeStickers();
+                gerador.create(inputStream, nomeArquivo, rating);
             }
-            System.out.println("\n");
         }
-       
     }
 }
--- src/GeradorDeStickers.java
+++ src/GeradorDeStickers.java
@@ -1,2 +1,82 @@
-package PACKAGE_NAME;public class GeradorDeStickers {
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class GeradorDeStickers {
+
+    public void create(InputStream inputStream, String nomeArquivo, Float rating) throws Exception{
+
+        // Leitura da imagem
+
+//        InputStream inputStream = new FileInputStream(new File("Assets/filme.jpg"));
+//        InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BMTY1NTI0ODUyOF5BMl5BanBnXkFtZTgwNTEyNjQ0MDE@.jpg");
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // Padronizar o tamanho
+        imagemOriginal= resizeImage(imagemOriginal, 750,1200);
+
+        // Cria nova imagem em memória com transparência e com tamanho novo
+
+        int width = imagemOriginal.getWidth();
+        int height = imagemOriginal.getHeight();
+        int newHeight = height+200;
+
+        BufferedImage novaImagem = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
+
+        // Copiar a imagem original para nova imagem
+
+        Graphics2D graphics = (Graphics2D)novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal,0,0, null);
+
+
+
+
+
+        // Configurar a fonte
+        Font fonte = new Font("Impact", Font.ITALIC, 86);
+
+        float ratio = width/newHeight;
+        int maxW = width-600;
+        int maxH = newHeight-400;
+        graphics.setFont(fonte);
+
+        // Escrever uma frase na nova imagem
+
+        if(rating>9) {
+            graphics.setColor(Color.GREEN.darker());
+            graphics.drawString("NOTA: "+rating, 70, newHeight - 100);
+            BufferedImage aproved = ImageIO.read(new File("Assets/approved300.png"));
+//             aproved.getScaledInstance(600, (int) (388/ratio), Image.SCALE_SMOOTH);
+            graphics.drawImage(aproved, width-300,newHeight-300, null);
+        }else{
+            graphics.setColor(Color.RED.darker());
+            graphics.drawString("NOTA: "+rating, 70, newHeight - 100);
+            BufferedImage aproved = ImageIO.read(new File("Assets/failed300.png"));
+//             aproved.getScaledInstance(600, (int) (388/ratio), Image.SCALE_SMOOTH);
+            graphics.drawImage(aproved, width-300,newHeight-300, null);
+        }
+
+        // Escrever a nova imagem em um arquivo
+        File sticker = new File("output/"+nomeArquivo);
+        if(sticker.mkdir()) {
+            ImageIO.write(novaImagem, "png", sticker);
+        }else{
+            System.out.println("File already exists");
+        }
+    }
+
+    BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+        Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_SMOOTH);
+        BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+        outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+        return outputImage;
+    }
+
+
 }
